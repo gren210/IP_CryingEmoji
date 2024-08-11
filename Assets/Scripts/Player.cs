e6749ddc825692b88c6574917ab4d5b1f049c2de
gren210 <s10257926@connect.np.edu.sg>
2024-08-12 01:01:43 +0800

    script update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using Cinemachine;
 using StarterAssets;
-using static UnityEditor.Experimental.GraphView.GraphView;
 using UnityEngine.Animations.Rigging;
 using UnityEngine.UIElements;
 
